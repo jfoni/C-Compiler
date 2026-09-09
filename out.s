1ee079e4bd48e57ded59c838943c5af969fc4471
jfoni <jannatuloni41@gmail.com>
2026-09-10 02:35:07 +0600

    Add assignment support

--- out.s
+++ out.s
@@ -10,23 +10,9 @@ main:
     push rax
     mov rax, 5
     pop rbx
-    mov rcx, rax
-    mov rax, rbx
-    cmp rax, rcx
-    setg al
-    movzx rax, al
-    cmp rax, 0
-    je .Lelse_0
+    add rax, rbx
+    mov [rbp - 8], rax
     mov rax, [rbp - 8]
-    push rax
-    mov rax, 2
-    pop rbx
-    imul rax, rbx
-    mov rsp, rbp
-    pop rbp
-    ret
-.Lelse_0:
-    mov rax, 0
     mov rsp, rbp
     pop rbp
     ret
--- test.c
+++ test.c
@@ -1,7 +1,5 @@
 int x = 10;
 
-if (x > 5) {
-    return x * 2;
-} else {
-    return 0;
-}
+x = x + 5;
+
+return x;
